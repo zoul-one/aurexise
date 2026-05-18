779b6d8f502eef77bbeb9d1ecb0dc2c5b8df7c52
Muhsina-V-P <muhsinavp1319@gmail.com>
2026-05-18 14:03:47 +0530

    feat:Create Jewellery Receipt Item from Purchase Receipt

--- aurexise/hooks.py
+++ aurexise/hooks.py
@@ -47,6 +47,7 @@ app_license = "mit"
 
 doctype_js = {
 	"Item": "public/js/item.js",
+	"Purchase Receipt": "public/js/purchase_receipt.js"
 }
 
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
